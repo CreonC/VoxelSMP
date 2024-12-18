4c5c41c1b40e1101a92b6c6b23d6f4db82477f5e
CreonC <69414582+CreonC@users.noreply.github.com>
2024-12-18 19:40:27 +0800

    remove feather

--- src/main/java/me/creonc/voxelsmp/events/BanFeather.java
+++ src/main/java/me/creonc/voxelsmp/events/BanFeather.java
@@ -11,15 +11,11 @@ public class BanFeather implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if (player.getClientBrandName().contains("Feather Fabric")) {
-            player.kickPlayer("Feather client is not allowed on this server.");
-        } else {
-            // Log to console the client brand name of the player if not using Feather Fabric
-            Bukkit.getLogger().info("[Voxel-Debug]: " + player.getName() + " is using " + player.getClientBrandName());
+        // Log to console the client brand name of the player if not using Feather Fabric
+        Bukkit.getLogger().info("[Voxel-Debug]: " + player.getName() + " is using " + player.getClientBrandName());
 
-            if (player.getClientBrandName().contains("Fabric")) { // Fabric can sometimes be used with other mods that creates an unfair advantage
-                player.sendMessage("Blacklisted modifications that create an unfair advantage are not allowed on this server.");
-            }
+        if (player.getClientBrandName().contains("Fabric")) { // Fabric can sometimes be used with other mods that creates an unfair advantage
+            player.sendMessage("Blacklisted modifications that create an unfair advantage are not allowed on this server.");
         }
     }
 }
